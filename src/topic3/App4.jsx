44225b416536af282bc970c65994abd494a78b96
Tanya Agrahari <rg96958227@gmail.com>
2025-11-01 18:47:39 +0530

    inline css in react

---
+++ src/topic3/App4.jsx
@@ -0,0 +1,131 @@
+function App4(){
+    const cardStyle = {
+        border:"1px solid #ccccc3b",
+        width:"300px",  
+        padding:"10px",
+        borderRadius:"10px",
+        boxShadow:"0 0 10px #ccc",
+    };
+
+    return (
+        <>
+        <h1  style={{color: 'red'}} > inline  css in react js</h1>
+        <div style={{display:'flex' ,flexWrap:'wrap', gap:'20px'}}>
+
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        <div style={cardStyle}>
+            <img style={ {width:'200px', }} src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0=" alt=""  />
+            <div>
+                <h4> Tanya Agrahari</h4>
+                <p> Software developer  </p>
+            </div>
+        </div>
+        </div>
+        </>
+    )
+}
+export default App4;
