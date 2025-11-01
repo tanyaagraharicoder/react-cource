5c9258157ac7f26db1aa6cc33826242c6baa692c
Tanya Agrahari <rg96958227@gmail.com>
2025-11-01 22:50:52 +0530

    external css i  react

---
+++ src/topic3/App6.jsx
@@ -0,0 +1,39 @@
+import "./css/style.css";
+
+function App6() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="heading">External Style</h1>
+
+        <div className="user-card">
+          <img
+            className="img-style"
+            src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0="
+            alt="Tanya Agrahari"
+            srcSet=""
+          />
+          <div className="text-wrap">
+            <h4>Tanya Agrahari</h4>
+            <p>Software Engineer</p>
+          </div>
+        </div>
+
+        <div className="user-card">
+          <img
+            className="img-style"
+            src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=612x612&w=is&k=20&c=qoSqKizQmeP0RByR-ARGs6W7JqVkNO17LlnXG_l6sc0="
+            alt="Tanya Agrahari"
+            srcSet=""
+          />
+          <div className="text-wrap">
+            <h4>Tanya Agrahari</h4>
+            <p>Software Engineer</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App6;
